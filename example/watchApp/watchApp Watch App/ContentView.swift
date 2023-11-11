85f5eda32d8fb556c1a0e233694b9503a83d00ef
Estiven Sánchez <estivensh4@gmail.com>
2023-11-11 03:44:50 -0500

    add watchApp module

---
+++ example/watchApp/watchApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchApp Watch App
+//
+//  Created by Estiven on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

adef50807002707429dadad43dd8a9981ad5ec1b
Estiven Sánchez <estivensh4@gmail.com>
2023-11-11 00:43:41 -0500

    iosApp and desktopApp modules are created for a sample project.

---
+++ example/build.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import com.android.build.gradle.BaseExtension
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath(moko.resourcesGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.4.0")
+    }
+}
+
+subprojects {
+    plugins.withType<com.android.build.gradle.BasePlugin> {
+        configure<BaseExtension> {
+            defaultConfig.minSdkVersion(16)
+            compileSdkVersion(33)
+        }
+    }
+}

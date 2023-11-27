e4402a8b4787ffedd94e5a3d220ca286edf289d6
Estiven Sánchez <estivensh4@gmail.com>
2023-11-27 18:18:11 -0500

    add plugin in dynamo

--- aws-dynamo/build.gradle.kts
+++ aws-dynamo/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinCocoapods)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.dokka)
     id("javadoc-stub-convention")
     id("publication-convention")
     id("detekt-convention")
